ee9f396166bd3d94877295b1d71b1ce1e4cd39a6
m3mpm <zgeorgan@cl-b5.nsk.21-school.ru>
2023-11-18 15:58:49 +0700

    init project

---
+++ src/main/java/com/learn/JavaSpringMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMvcThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringMvcThymeleafApplication.class, args);
+    }
+
+}
